df6a1474f4c97eb879052ca93067d484115fa80b
md <mattyoott@gmail.com>
2026-02-13 02:47:46 +1300

    Added basic structure

--- Program.cs
+++ Program.cs
@@ -2,9 +2,37 @@
 {
     internal class Program
     {
+        //Global variables
+
+
+
+        //Main method
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Wage Calculator\nEnter to continue");
+            Console.ReadLine();
+
+
+
+            //Console.WriteLine("You have entered {} Employees.");
         }
+
+
+        //One Employee method
+        static string OneEmployee()
+        {
+
+
+
+
+
+            return "";
+        }
+
+
+
+
+
+
     }
 }
