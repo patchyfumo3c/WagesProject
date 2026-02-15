14968dda3ce7c6b7152ea3956a1f5f815a454f35
md <mattyoott@gmail.com>
2026-02-16 12:00:13 +1300

    Added const variables

--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
 ﻿namespace WagesApp
 {
-    internal class Program
+    public class Program
     {
         //Global variables
 
-
-
+        // Constant variables
+        public const float PAYRATE = 22.0f;
+        public static readonly IReadOnlyCollection<float> TAXRATES = new List<float> { 0.075f, 0.07f }.AsReadOnly();
+        public static readonly int BONUS = 5, BONUSCONDITION = 30;
         //Main method
         static void Main(string[] args)
         {
-            Console.WriteLine("Wage Calculator\nEnter to continue");
+            //Display app title screen
+            Console.WriteLine("                                                                                          \r\n                                                                                          \r\n██     ██  ▄▄▄   ▄▄▄▄ ▄▄▄▄▄   ▄█████  ▄▄▄  ▄▄     ▄▄▄▄ ▄▄ ▄▄ ▄▄     ▄▄▄ ▄▄▄▄▄▄ ▄▄▄  ▄▄▄▄  \r\n██ ▄█▄ ██ ██▀██ ██ ▄▄ ██▄▄    ██     ██▀██ ██    ██▀▀▀ ██ ██ ██    ██▀██  ██  ██▀██ ██▄█▄ \r\n ▀██▀██▀  ██▀██ ▀███▀ ██▄▄▄   ▀█████ ██▀██ ██▄▄▄ ▀████ ▀███▀ ██▄▄▄ ██▀██  ██  ▀███▀ ██ ██ \r\n                                                                                          ");
+            Console.WriteLine("Enter to continue");
             Console.ReadLine();
 
 
